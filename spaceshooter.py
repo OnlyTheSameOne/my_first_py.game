9a9cfec5378673f1a4a40fdc72c89fd9d3f8827c
OnlyTheSameOne <79103741+OnlyTheSameOne@users.noreply.github.com>
2024-11-17 20:38:36 +0100

    Uplaod the image

--- spaceshooter.py
+++ spaceshooter.py
@@ -15,16 +15,38 @@ gameDisplay = pygame.display.set_mode((display_height,display_width))
 pygame.display.set_caption("Space Shooter")
 #FPS bzw Ticks pro sekunde
 clock = pygame.time.Clock()
+#generiere das Space Ship in den Hintergrund
+spaceShip = pygame.image.load("puprle_ship.png")
+
+#Funktion die Das SPaceShip mit der blit methode an dern psoition x & y erzeugt 
+#dabei ist spaceShip ein Parameter mit den werten von (x,y)
+def ship(x,y):
+    gameDisplay.blit(spaceShip,(x,y))
+
+#Das kordinaten-System in Pygame starte oben link vom Bildschirm P(0/0)
+#Positve X werte bringen den den Punkt nach rechts
+#Positive Y werte bringen den Punk nach links
+x = (display_width * 0.45)
+y =  (display_height * 0.8) 
+
+
+
+
+
 #Das Raumschiff is nicht zerstört
 exploded = False 
 
 while not exploded:
-#Erzeugt eine Liste mit Events die PAssiern pro Frame pro Sekunde ist eine Pygame methode
+#Erzeugt eine Liste mit Events die Passiern pro Frame pro Sekunde ist eine Pygame methode
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exploded = True
 # Gibt die Evnets zu Kontrolle in der Konsole aus
-        print(event)
+       #print(event)
+#Zuerst erzeugen wir den weißen Hintergrund
+    gameDisplay.fill(white)
+#Danch Erzeugen wir das Space Ship    
+    ship(x,y)
 #mit dieser methode kann man alle und spezifische parameter aktuallisieren
     pygame.display.update()
 
